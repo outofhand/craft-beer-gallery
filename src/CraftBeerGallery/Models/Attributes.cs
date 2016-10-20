ef6acb1138c9c79f2d953ec6a6c780d9770efc43
outofhand <rnoroian@gmail.com>
2016-10-20 17:41:54 -0400

    Added Beer and Attributes models

---
+++ src/CraftBeerGallery/Models/Attributes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CraftBeerGallery.Models
+{
+    public class Attributes
+    {
+        public string Appearance { get; set; }
+        public string Smell { get; set; }
+        public string Taste { get; set; }
+        public string Mouthfeel { get; set; }
+        public string Overall { get; set; }
+    }
+}
---
+++ src/CraftBeerGallery/Models/Beer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CraftBeerGallery.Models
+{
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string BeerName { get; set; }
+        public string Brewery { get; set; }
+        public string BreweryLocation { get; set; }
+        public string Style { get; set; }
+        public int ABV { get; set; }
+        public string DescriptionHtml { get; set; }
+        public string Availability { get; set; }
+        public Attributes[] BeerAttributes { get; set; }
+        public bool Favorite { get; set; }
+    }
+
+}
