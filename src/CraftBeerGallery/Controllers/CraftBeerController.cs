a66267293651ae813f16b54cc091e9537260677d
outofhand <rnoroian@gmail.com>
2016-10-20 16:42:24 -0400

    Updated Beer Detail view

--- src/CraftBeerGallery/Controllers/CraftBeerController.cs
+++ src/CraftBeerGallery/Controllers/CraftBeerController.cs
@@ -10,6 +10,20 @@ namespace CraftBeerGallery.Controllers
     {
         public ActionResult Detail()
         {
+
+            ViewBag.Brewery = "Clown Shoes";
+            ViewBag.Beer = "The Barista";
+            ViewBag.Year = "2016";
+            ViewBag.Description = "<p><strong>The Barista Breakfast Brown</strong> is crafted with Maris Otter malt, flaked oatmeal, cold brewed espresso, milk sugar and the spirit of angel wings.</p>";
+            ViewBag.BeerDetails = new string[]
+            {
+                "style: brown ale",
+                "brewdate: 2016",
+                "brewery: Clown Shoes",
+                "size: 22 Oz Bombers",
+                "availability: available"
+            };
+
             return View();
         }
     }
